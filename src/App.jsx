6fbe5ebd9c6c11ff843ec7e0193a6a4cc5ac8628
Dariusz Okonski <darek200180@gmail.com>
2026-09-01 06:36:20 +0200

    add toggle button to NoteForm component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,50 @@ import NoteForm from "./components/NoteForm";
 import NoteList from "./components/NoteList";
 
 function App() {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState([
+    // {
+    //   id: 1788236021561,
+    //   title: "aaa",
+    //   priority: "Medium",
+    //   category: "Work",
+    //   description: "aaab",
+    // },
+    // {
+    //   id: 1788236029352,
+    //   title: "bbbb",
+    //   category: "Work",
+    //   priority: "Medium",
+    //   description: "bbbad",
+    // },
+    // {
+    //   id: 1788236710593,
+    //   title: "safdasdf",
+    //   priority: "High",
+    //   category: "Ideas",
+    //   description: "asdfasdfasfd",
+    // },
+    // {
+    //   id: 1788236714980,
+    //   title: "asdfasdf",
+    //   category: "Work",
+    //   priority: "Medium",
+    //   description: "asdfafsdasdfasf",
+    // },
+    // {
+    //   id: 1788236717988,
+    //   title: "asdfasfdasfdasfd",
+    //   category: "Work",
+    //   priority: "Medium",
+    //   description: "asdfasfaf",
+    // },
+    // {
+    //   id: 1788236720696,
+    //   title: "asdfasfasfd",
+    //   category: "Work",
+    //   priority: "Medium",
+    //   description: "asdfasfdasdf",
+    // },
+  ]);
 
   return (
     <div className="max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg">
