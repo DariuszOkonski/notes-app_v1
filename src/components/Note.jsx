1d09253e8f56467e46cf93858b434eb66b21ca39
Dariusz Okonski <darek200180@gmail.com>
2026-09-02 07:54:26 +0200

    add Note component

---
+++ src/components/Note.jsx
@@ -0,0 +1,42 @@
+const Note = ({ note, onDeleteNote }) => {
+  const handleDeleteNote = (id) => {
+    onDeleteNote(id);
+  };
+
+  const borderLeftColor =
+    note.priority === "High"
+      ? "red"
+      : note.priority === "Medium"
+        ? "orange"
+        : "green";
+  return (
+    <div
+      key={note.id}
+      className="p-4 rounded-lg shadow-md bg-white border-l-4"
+      style={{ borderLeftColor }}
+    >
+      <h3 className="text-lg font-bold">{note.title}</h3>
+      <p className="text-sm text-gray-600">
+        <strong>Category: </strong>
+        {note.category}
+      </p>
+      <p className="text-sm text-gray-600">
+        <strong>Priority: </strong>
+        {note.priority}
+      </p>
+      <p className="text-sm text-gray-600">
+        <strong>Description: </strong>
+        {note.description}
+      </p>
+
+      <button
+        onClick={() => handleDeleteNote(note.id)}
+        className="mt-3 text-red-500 cursor-pointer transition hover:text-red-700"
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default Note;
--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,50 +1,14 @@
+import Note from "./Note";
+
 const NoteList = ({ notes, onDeleteNote }) => {
   if (notes.length === 0) {
     return <p className="text-center text-gray-500">No Notes Yet</p>;
   }
 
-  const handleDeleteNote = (id) => {
-    onDeleteNote(id);
-  };
-
   return (
     <div className="space-y-4">
       {notes.map((note) => {
-        const borderLeftColor =
-          note.priority === "High"
-            ? "red"
-            : note.priority === "Medium"
-              ? "orange"
-              : "green";
-
-        return (
-          <div
-            key={note.id}
-            className="p-4 rounded-lg shadow-md bg-white border-l-4"
-            style={{ borderLeftColor }}
-          >
-            <h3 className="text-lg font-bold">{note.title}</h3>
-            <p className="text-sm text-gray-600">
-              <strong>Category: </strong>
-              {note.category}
-            </p>
-            <p className="text-sm text-gray-600">
-              <strong>Priority: </strong>
-              {note.priority}
-            </p>
-            <p className="text-sm text-gray-600">
-              <strong>Description: </strong>
-              {note.description}
-            </p>
-
-            <button
-              onClick={() => handleDeleteNote(note.id)}
-              className="mt-3 text-red-500 cursor-pointer transition hover:text-red-700"
-            >
-              Delete
-            </button>
-          </div>
-        );
+        return <Note key={note.id} note={note} onDeleteNote={onDeleteNote} />;
       })}
     </div>
   );
