5780b1ae117390e2f0cd6f210f1c2f77e103fca9
Dariusz Okonski <darek200180@gmail.com>
2026-09-03 08:14:45 +0200

    add read from localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NoteForm from "./components/NoteForm";
 import NoteList from "./components/NoteList";
 
 function App() {
-  const [notes, setNotes] = useState([
-    // {
-    //   id: 1788236021561,
-    //   title: "aaa",
-    //   priority: "High",
-    //   category: "Work",
-    //   description: "aaab",
-    // },
-    // {
-    //   id: 1788236029352,
-    //   title: "bbbb",
-    //   category: "Work",
-    //   priority: "Medium",
-    //   description: "bbbad",
-    // },
-    // {
-    //   id: 1788236710593,
-    //   title: "safdasdf",
-    //   priority: "Low",
-    //   category: "Ideas",
-    //   description: "asdfasdfasfd",
-    // },
-    // {
-    //   id: 1788236714980,
-    //   title: "asdfasdf",
-    //   category: "Work",
-    //   priority: "Medium",
-    //   description: "asdfafsdasdfasf",
-    // },
-    // {
-    //   id: 1788236717988,
-    //   title: "asdfasfdasfdasfd",
-    //   category: "Work",
-    //   priority: "Medium",
-    //   description: "asdfasfaf",
-    // },
-    // {
-    //   id: 1788236720696,
-    //   title: "asdfasfasfd",
-    //   category: "Work",
-    //   priority: "Medium",
-    //   description: "asdfasfdasdf",
-    // },
-  ]);
+  const [notes, setNotes] = useState(() => {
+    return JSON.parse(localStorage.getItem("notes")) ?? [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
 
   const deleteNote = (id) => {
     const confirmDelete = window.confirm(
